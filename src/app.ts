40aba4400adcb24730ec2b5dc08bb87bb5080668
Diljot Singh Sangha <dsangha2@academic.rrc.ca>
2025-03-23 19:32:07 -0500

    feat: configure CORS with trusted origins

--- src/app.ts
+++ src/app.ts
@@ -4,14 +4,23 @@ import setupSwagger from "../config/swagger";
 import employeeRoutes from "./api/v1/routes/employeeRoutes";
 import branchRoutes from "./api/v1/routes/branchRoutes";
 import errorHandler from "./api/v1/middleware/errorHandler";
-import dotenv from "dotenv"
-import helmet from "helmet"
+import dotenv from "dotenv";
+import helmet from "helmet";
+import cors from "cors";
+import { CorsOptions } from 'cors';
 
 dotenv.config();
 
 const app: Express = express();
 
 app.use(helmet());
+
+const corsOptions: CorsOptions = {
+	origin: ["https://trusted-domain.com", "http://localhost:3000"],
+	optionsSuccessStatus: 200
+  };
+
+app.use(cors(corsOptions));
 
 app.use(express.json());
 
